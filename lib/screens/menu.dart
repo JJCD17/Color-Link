1cb177ddb6f4c477106b01804f3366e656b94b77
Jonathan Dominguez <jjcd1706@gmail.com>
2025-05-13 21:23:50 -0600

    Se hacen cambios generales para limpiar codigo y ejorar funcionamiento

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../widgets/game_board.dart';
 import '../models/game_stats_storage.dart';
 
