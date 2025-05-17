e2c06e41effe8c76a47a64a32449565f6f57b63f
Jonathan Dominguez <jjcd1706@gmail.com>
2025-05-16 19:15:56 -0600

    Se crea metodo nuevo que genera la celda vacia de manera correcta

--- lib/models/point_data.dart
+++ lib/models/point_data.dart
@@ -4,6 +4,7 @@ class PointData {
   final int row;
   final int col;
   Color? color;
+  final bool isEmpty;
   bool isSelected;
   bool isVisible;
 
@@ -11,7 +12,18 @@ class PointData {
     required this.row,
     required this.col,
     required this.color,
+    this.isEmpty = false,
     this.isSelected = false,
     this.isVisible = true,
   });
+
+  // Método factory para crear una celda vacía
+  factory PointData.empty({int row = -1, int col = -1}) {
+    return PointData(
+      row: row,
+      col: col,
+      color: null,
+      isEmpty: true,
+    );
+  }
 }
